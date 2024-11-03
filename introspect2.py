2ab097d29d0a5a1f44a0398431d0b9eaf2516268
ps21 <Dyubchenkovyaycheslav@>
2024-11-03 10:15:22 +0200

    jj

--- introspect2.py
+++ introspect2.py
@@ -1,3 +1,10 @@
-date = "string"
-print(hasattr(data,"reverse"))
-print(hasattr(data,"index"))
+data = "string"
+print(hasattr(data, "reverse"))
+print(hasattr(data, "index"))
+
+print(f'!!! {data.startswith("st")} !!!')
+
+starts = getattr(data, "startswith",'st')
+
+
+print(type(starts))
